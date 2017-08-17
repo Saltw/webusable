1b7b1cb4f23c495235d4ab05d4efa122f20b4362
Saltw <root.webusable@gmail.com>
2017-08-18 00:45:24 +0200

    Añade un title_on y un title_off al checkmark
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   includes/smarty/intelli_plugins/function.html_radio_switcher.php
    
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   admin/templates/default/Gulpfile.js
    	modified:   admin/templates/default/css/bootstrap-calmy.css
    	modified:   admin/templates/default/css/bootstrap-darkness.css
    	modified:   admin/templates/default/css/bootstrap-default.css
    	modified:   admin/templates/default/css/bootstrap-gebeus-waterfall.css
    	modified:   admin/templates/default/css/bootstrap-radiant-orchid.css
    	modified:   admin/templates/default/css/bootstrap-roseus.css
    	modified:   admin/templates/default/fields.tpl
    	modified:   admin/templates/default/less/bootstrap/forms.less
    	modified:   admin/templates/default/less/bootstrap/variables.less
    	modified:   admin/templates/default/package.json
    	modified:   js/admin/configuration.js
    	modified:   templates/_common/_seo/structured-data.tpl
    	modified:   templates/kickstart/css/application.css
    	modified:   templates/kickstart/layout.tpl
    	modified:   templates/kickstart/sass/cliente/theme/_colores.scss
    	modified:   templates/kickstart/sass/cliente/theme/_tema.scss

--- includes/smarty/intelli_plugins/function.html_radio_switcher.php
+++ includes/smarty/intelli_plugins/function.html_radio_switcher.php
@@ -17,9 +17,24 @@ function smarty_function_html_radio_switcher(array $params)
 		$attr .= ' disabled';
 	}
 
+	$title_on = NULL;
+	$title_off = NULL;
+
+    if (isset($params['titleOn']))
+	{
+		$titleOn = $params['titleOn'];
+		$title_on = "title='{$titleOn}'";
+	}
+
+	if (isset($params['titleOff']))
+	{
+		$titleOff = $params['titleOff'];
+		$title_off = "title='{$titleOff}'";
+	}
+
 	echo <<<OUT
 <input type="hidden" value="0" name="{$name}">
-<div class="js-input-switch make-switch" id="switch-{$id}" data-animated="false" data-on="success" data-off="danger" data-on-label="&lt;i class='i-checkmark'&gt;&lt;/i&gt;" data-off-label="&lt;i class='i-close'&gt;&lt;/i&gt;">
+<div class="js-input-switch make-switch" id="switch-{$id}" data-animated="false" data-on="success" data-off="danger" data-on-label="&lt;i class='i-checkmark' {$title_on} &gt;&lt;/i&gt;" data-off-label="&lt;i class='i-close' {$title_off} &gt;&lt;/i&gt;">
 	<input type="checkbox" name="{$name}" id="{$id}" value="{$value}"{$attr}>
 </div>
 OUT;
